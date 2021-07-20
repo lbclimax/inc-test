0b10ef3346849a1e64f2d10ba5d4f7e625bd5f29
root <root@lbclimax.ml>
2021-07-21 00:40:29 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,2 @@
+net = require('net');
+
---
+++ server.js
@@ -0,0 +1,32 @@
+var net = require('net');
+var id = 1 ;
+
+var server = net.createServer(function(socket) {
+	
+	let uid = id++;
+	
+	console.log(`client conected with id  ${uid} `)
+	
+	socket.on('data',(data)=>{
+		
+		console.log(`>>${uid}:${data.toString()}`);
+	})
+
+	socket.on("error",(err)=>{
+		console.log("error occured on client id ",uid);
+		console.log(err.stack);
+
+	})
+
+	
+	
+	socket.pipe(socket);
+	
+});
+server.listen(8081, '0.0.0.0');
+
+server.on('error',(err)=>{
+	console.log('server error occured ');
+	console.log(err.stack);
+});
+
